cc55c5f7fe4572fe9e9b93f267f9815883759909
Micah Thornton <mathornton@smu.edu>
2021-05-08 14:41:15 -0500

    Now will permute the answers

--- R/ham-exam.R
+++ R/ham-exam.R
@@ -4,11 +4,29 @@ setwd("C:/Users/matho/Desktop/HAM-Questions")
 
 askOneQ <- function(QDF,probvec = rep(1/nrow(QDF),nrow(QDF))){
   qidx <- sample(1:nrow(QDF),1,prob=probvec); 
+  
+  qans1 <- substr(as.character(QDF[qidx,3]),3,nchar(as.character(QDF[qidx,3])))
+  qans2 <- substr(as.character(QDF[qidx,4]),3,nchar(as.character(QDF[qidx,4])))
+  qans3 <- substr(as.character(QDF[qidx,5]),3,nchar(as.character(QDF[qidx,5])))
+  qans4 <- substr(as.character(QDF[qidx,6]),3,nchar(as.character(QDF[qidx,6])))
+  
+  qans <- c(qans1,qans2,qans3,qans4)
+  qmix <- sample(qans)
+  
+  oldsolnval <- QDF[qidx,7];
+  if (oldsolnval == "(A)"){oldsolnidx <- 1;}
+  if (oldsolnval == "(B)"){oldsolnidx <- 2;}
+  if (oldsolnval == "(C)"){oldsolnidx <- 3;}
+  if (oldsolnval == "(D)"){oldsolnidx <- 4;}
+  
+  newsolnval <- c("(A)","(B)","(C)","(D)")[which(qans[oldsolnidx] == qmix)];
+  
   cat(paste(as.character(QDF[qidx,1]),"|", as.character(QDF[qidx,2])),"\n");
-  cat(paste("\t", as.character(QDF[qidx,3])),"\n");
-  cat(paste("\t", as.character(QDF[qidx,4])),"\n");
-  cat(paste("\t", as.character(QDF[qidx,5])),"\n");
-  cat(paste("\t", as.character(QDF[qidx,6])),"\n");
+  cat(paste("\t","A.",qmix[1],"\n"));
+  cat(paste("\t","B.",qmix[2],"\n"));
+  cat(paste("\t","C.",qmix[3],"\n"));
+  cat(paste("\t","D.",qmix[4],"\n"));
+  
   ans <- readline("Please Enter letter of answer (A-D) [Q to quit]: ")
   while(!(toupper(ans) %in% c("A","B","C","D","Q"))){
     ans <- readline("Please Enter letter of answer (A-D) [Q to quit]: ")
@@ -16,10 +34,10 @@ askOneQ <- function(QDF,probvec = rep(1/nrow(QDF),nrow(QDF))){
   if(toupper(ans) == "Q"){
     return(list(qu=1,probvec))
   }
-  corans <- grep(toupper(ans),QDF[qidx,7]);
+  corans <- grep(toupper(ans),newsolnval);
   pvec <- probvec*nrow(QDF); 
   if (length(corans) != 1){
-    cat("Sorry, that is incorrect, upweighting selection probability\n")
+    cat(paste("Sorry, that is incorrect, upweighting selection probability \n The correct solution was: ", newsolnval, qans[oldsolnidx],"\n"))
     pvec[qidx] <- pvec[qidx]+(0.5*pvec[qidx]);
   } else if (corans == 1){
     cat("Correct Answer, Downweighting selection probability\n"); 
@@ -40,5 +58,10 @@ ContinuallyAskQs <- function(QDF,probvec = rep(1/nrow(QDF),nrow(QDF))){
 }
 
 generalQs <- read.csv("genquest.csv",sep="|");
+QDF <- generalQs;
 
-ContinuallyAskQs(QDF)
+numto <- 45; 
+probvec <- c(rep(1,numto),rep(0,nrow(QDF)-numto))
+probvec <- probvec/sum(probvec)
+
+ContinuallyAskQs(QDF,probvec)
